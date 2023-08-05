29172de429ceb08c29d5ce96d611783b9223406d
Jpwrbd <jpwrbd456@gmail.com>
2023-08-05 16:29:24 +0700

    up

--- Homework/HW04/hw04-3-basic-operator.cpp
+++ Homework/HW04/hw04-3-basic-operator.cpp
@@ -1,15 +1,24 @@
 
 /*
     จงกรอกหมายเลขทั้งหมดจำนวน 3 ค่า และแสดงผลรวมดังแสดงดังผลลัพธ์ด้านล่าง
-    
     Test case:
         2 3 1
     Output:
         Answer = 6
-
     Test case:
         4 -1 3
     Output:
         Answer = 6
 */
 #include <stdio.h>
+int main()
+{
+    int Num1;
+    int Num2;
+    int Num3;
+    int Answer;
+    scanf("%d %d %d", &Num1, &Num2, &Num3);
+    Answer = Num1 + Num2 + Num3;
+    printf("Answer = %d", Answer);
+    return 0;
+} // endfunction
